86c1747db4993eafdcfd763a4c7fdfbeef2220c7
SimoneCerri <simone.cerri1994@gmail.com>
2024-06-18 15:39:49 +0200

    here we are, front-end

---
+++ src/state.js
@@ -0,0 +1,22 @@
+import { reactive } from "vue";
+import axios from "axios";
+
+export const state = reactive({
+
+    base_url: "http://127.0.0.1:8000",
+    doctors_url: "/api/doctors",
+
+
+    doctors: [],
+
+
+    getDoctors(url) {
+        axios.get(url)
+            .then(response => {
+                //console.log(response.data.doctors.data);
+                this.doctors = response.data.doctors.data;
+                // console.log();
+            })//add errors and messages
+    },
+
+})
