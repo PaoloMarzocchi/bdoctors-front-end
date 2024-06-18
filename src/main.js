d3bea5430be01934c041529105641b2dd88b7d46
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-06-18 18:41:25 +0200

    fix an error in main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
-import { router } from './router';
+
 import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
-
+import router from './router.js';
 createApp(App).use(router).mount('#app')
 
