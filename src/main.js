9b5cfa73a8f113d3a6843259e6ff4dd66b66338d
SimoneCerri <simone.cerri1994@gmail.com>
2024-06-24 11:39:23 +0200

    fontawesome

--- src/main.js
+++ src/main.js
@@ -5,5 +5,11 @@ import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import router from './router.js';
-createApp(App).use(router).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faStar } '@fortawesome/free-solid-svg-icons';
+
+library.add(faStar);
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router).mount('#app')
 
--- src/views/VoteDoctor.vue
+++ src/views/VoteDoctor.vue
@@ -6,15 +6,40 @@ export default {
     data() {
         return {
             state,
+            vote: null,
+            loading: false,
+        }
+    },
+    methods:
+    {
+        sendVote() {
+            this.loading = true;
+            /* this.vote = ; */
         }
     }
 }
 </script>
 
 <template>
-    <div>
-        Qui
-    </div>
+    <main>
+        <div class="container my-5">
+            <form @submit.prevent="sendVote()" action="">
+                <span v-for="n in 5" class="vote_stars">
+                    <font-awesome-icon icon="fa-regular fa-star" size="lg" style="color: #FFD43B;" />
+                </span>
+                <button type="submit" class="btn btn-dark text-success" :disabled="loading">Send</button>
+                <button @click="$router.back()" class="btn btn-dark text-warning">Cancel</button>
+            </form>
+        </div>
+    </main>
 </template>
 
-<style></style>
+<style>
+
+.vote_stars {
+    color: gold;
+
+}
+
+.vote_stars:hover { }
+</style>
