d58920ad30f12ce161ca02c49311ea194a36d5e1
SimoneCerri <simone.cerri1994@gmail.com>
2024-06-27 18:07:02 +0200

    modify form's title, no more big buttons, change some classes as well

--- src/components/MessageForm.vue
+++ src/components/MessageForm.vue
@@ -1,9 +1,9 @@
 <script>
-import { state } from '../state';
+import { state } from "../state";
 import axios from "axios";
 
 export default {
-  name: 'MessageForm',
+  name: "MessageForm",
   props: {
     doc_id: Number,
   },
@@ -16,21 +16,20 @@ export default {
 
       // Form variables
       doctor_profile_id: this.doc_id,
-      sender_first_name: '',
-      sender_last_name: '',
-      email: '',
-      message: '',
-      sender_first_nameError: '',
-      sender_last_nameError: '',
-      emailError: '',
-      messageError: '',
+      sender_first_name: "",
+      sender_last_name: "",
+      email: "",
+      message: "",
+      sender_first_nameError: "",
+      sender_last_nameError: "",
+      emailError: "",
+      messageError: "",
       loading: false,
       success: false,
-      errors: false
-    }
+      errors: false,
+    };
   },
   methods: {
-
     sendMessage() {
       this.loading = true;
 
@@ -46,132 +45,127 @@ export default {
 
       axios
         .post(this.url, data)
-        .then(response => {
+        .then((response) => {
           console.log(response);
           console.log(response.data);
           console.log(response.data.errors);
 
           if (response.data.success) {
-            this.sender_first_name = '',
-              this.sender_last_name = '',
-              this.email = '',
-              this.message = '',
-              this.success = response.data.message;
+            (this.sender_first_name = ""),
+              (this.sender_last_name = ""),
+              (this.email = ""),
+              (this.message = ""),
+              (this.success = response.data.message);
           } else {
-            this.firstNameError = response.data.errors.sender_first_name,
-              this.lastNameError = response.data.errors.sender_last_name,
-              this.emailError = response.data.errors.email,
-              this.messageError = response.data.errors.message_text;
+            (this.firstNameError = response.data.errors.sender_first_name),
+              (this.lastNameError = response.data.errors.sender_last_name),
+              (this.emailError = response.data.errors.email),
+              (this.messageError = response.data.errors.message_text);
           }
 
           this.loading = false;
-
         })
-        .catch(err => {
+        .catch((err) => {
           console.log(err);
         });
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <template>
   <div class="message_form position-relative mt-5">
     <div class="shadow-lg rounded-4 p-4">
-
-      <div class="message_form_title rounded-4 position-relative d-flex justify-content-center align-items-center">
-        <h2>Leave a message</h2>
+      <div class="d-flex align-items-center justify-content-center">
+        <div class="my_form_title p-4">
+          <h2>Leave a message</h2>
+        </div>
       </div>
+      <div class="card-body bg-light rounded-4 p-4">
+        <template v-if="success">
+          <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            <strong>Thank you!</strong>
+            <br />
+            <span>Your review has been sent!</span>
+          </div>
+        </template>
 
-      <template v-if="success">
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-          <strong>Thank you!</strong>
-          <br>
-          <span>Your review has been sent!</span>
-        </div>
-      </template>
+        <template v-if="errors">
+          <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            <strong class="">Errors</strong>
+            <ul class="list-unstyled">
+              <li class="text-danger" v-for="error in errors">
+                <span class="">{{ error[0] }}</span>
+              </li>
+            </ul>
+          </div>
+        </template>
 
-      <template v-if="errors">
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-          <strong class="">Errors</strong>
-          <ul class="list-unstyled ">
-            <li class="text-danger " v-for="error in errors">
-              <span class="">{{ error[0] }}</span>
-            </li>
-          </ul>
-        </div>
-      </template>
+        <form @submit.prevent="sendMessage()" class="row g-3">
+          <div class="col-12 col-md-6 form-floating">
+            <input type="text" class="form-control" id="sender_first_name" aria-describedby="helpId"
+              placeholder="Your first name here" v-model="sender_first_name" />
 
-      <form @submit.prevent="sendMessage()" class="row g-3">
+            <label for="sender_first_name" class="ms-2">First Name *</label>
 
-        <div class="col-12 col-md-6 form-floating">
-          <input type="text" class="form-control" id="sender_first_name" aria-describedby="helpId"
-            placeholder="Your first name here" v-model="sender_first_name" />
+            <p class="text-danger mt-3" v-show="this.sender_first_nameError">
+              {{ this.sender_first_nameError }}
+            </p>
+            <small id="helpId" class="form-text text-muted">Type your first name here <i
+                class="fa-solid fa-arrow-up"></i></small>
+          </div>
 
-          <label for="sender_first_name" class="ms-2">First Name *</label>
+          <div class="col-12 col-md-6 form-floating">
+            <input type="text" class="form-control" name="sender_last_name" id="sender_last_name"
+              aria-describedby="helpId" placeholder="Your last name here" v-model="sender_last_name" />
+            <label for="sender_last_name" class="ms-2">Last Name *</label>
 
-          <p class="text-danger mt-3" v-show="this.sender_first_nameError">
-            {{ this.sender_first_nameError }}
-          </p>
-          <small id="helpId" class="form-text text-muted">Type your first name here <i
-              class="fa-solid fa-arrow-up"></i></small>
+            <p class="text-danger mt-3" v-show="this.sender_last_nameError">
+              {{ this.sender_last_nameError }}
+            </p>
+            <small id="helpId" class="form-text text-muted">Type your last name here <i
+                class="fa-solid fa-arrow-up"></i></small>
+          </div>
 
-        </div>
+          <div class="col-12 form-floating">
+            <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelpId"
+              placeholder="abc@mail.com" v-model="email" />
+            <label for="email" class="ms-2">Email *</label>
 
-        <div class="col-12 col-md-6 form-floating">
-          <input type="text" class="form-control" name="sender_last_name" id="sender_last_name"
-            aria-describedby="helpId" placeholder="Your last name here" v-model="sender_last_name" />
-          <label for="sender_last_name" class="ms-2">Last Name *</label>
+            <p class="text-danger mt-3" v-show="this.emailError">
+              {{ this.emailError }}
+            </p>
+            <small id="emailHelpId" class="form-text text-muted">Insert your email here <i
+                class="fa-solid fa-arrow-up"></i></small>
+          </div>
 
-          <p class="text-danger mt-3" v-show="this.sender_last_nameError">
-            {{ this.sender_last_nameError }}
-          </p>
-          <small id="helpId" class="form-text text-muted">Type your last name here <i
-              class="fa-solid fa-arrow-up"></i></small>
+          <div class="col-12 form-floating">
+            <textarea class="form-control" placeholder="Leave a message here" name="message" id="message" rows="10"
+              style="height: 100px" v-model="message"></textarea>
+            <label for="message" class="ms-2">Your message *</label>
 
-        </div>
+            <p class="text-danger mt-3" v-show="this.messageError">
+              {{ this.messageError }}
+            </p>
+            <small id="messageHelpId" class="form-text text-muted">Write here what you want to tell me <i
+                class="fa-solid fa-arrow-up"></i></small>
+          </div>
 
-        <div class="col-12 form-floating">
-          <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelpId"
-            placeholder="abc@mail.com" v-model="email" />
-          <label for="email" class="ms-2">Email *</label>
+          <div class="col-md-12 mt-5 row text-danger">
+            <p>( <span class="text-dark">*</span> ) Required fields.</p>
+          </div>
 
-          <p class="text-danger mt-3" v-show="this.emailError">
-            {{ this.emailError }}
-          </p>
-          <small id="emailHelpId" class="form-text text-muted">Insert your email here <i
-              class="fa-solid fa-arrow-up"></i></small>
-
-        </div>
-
-        <div class="col-12 form-floating">
-          <textarea class="form-control" placeholder="Leave a message here" name="message" id="message" rows="10"
-            style="height: 100px" v-model="message"></textarea>
-          <label for="message" class="ms-2">Your message *</label>
-
-          <p class="text-danger mt-3" v-show="this.messageError">
-            {{ this.messageError }}
-          </p>
-          <small id="messageHelpId" class="form-text text-muted">Write here what you want to tell me
-            <i class="fa-solid fa-arrow-up"></i></small>
-
-        </div>
-
-        <div class="col-md-12 mt-5 row text-danger">
-          <p>( <span class="text-dark">*</span> ) Required fields.</p>
-        </div>
-
-        <div class="col-12 d-flex justify-content-center align-items-center">
-          <button type="submit" class="btn btn-primary btn_message" :disabled="loading">
-            {{ loading ? 'Sending message...' : 'Send message' }}
-          </button>
-        </div>
-      </form>
+          <div class="col-12 d-flex justify-content-center align-items-center">
+            <button type="submit" class="btn btn-primary btn_message" :disabled="loading">
+              {{ loading ? "Sending message..." : "Send message" }}
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
-
 </template>
 
 <style scoped>
@@ -201,7 +195,7 @@ export default {
   }
 
   &:hover::before {
-    opacity: 1
+    opacity: 1;
   }
 }
 
@@ -218,6 +212,58 @@ export default {
     font-weight: bold;
     color: #fff;
     font-size: 2rem;
+  }
+}
+
+.my_form_title {
+  font-size: 35px;
+  color: #f77b02;
+  font-weight: 600;
+  letter-spacing: -1px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  padding-left: 30px;
+
+  h2 {
+    font-weight: bold;
+    font-size: 3rem;
+  }
+}
+
+.my_form_title::before,
+.my_form_title::after {
+  position: absolute;
+  content: "";
+  height: 20px;
+  width: 20px;
+  border-radius: 50%;
+  left: -10px;
+  bottom: 44%;
+  background-color: #f77b02;
+}
+
+.my_form_title::before {
+  width: 20px;
+  height: 20px;
+  background-color: #f77b02;
+}
+
+.my_form_title::after {
+  width: 20px;
+  height: 20px;
+  animation: pulse 1s linear infinite;
+}
+
+@keyframes pulse {
+  from {
+    transform: scale(1);
+    opacity: 1;
+  }
+
+  to {
+    transform: scale(2);
+    opacity: 0;
   }
 }
 </style>
