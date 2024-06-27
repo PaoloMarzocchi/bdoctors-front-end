0f128b7d8cde4861bc0d40ae46f45ff9983f2d3c
SimoneCerri <simone.cerri1994@gmail.com>
2024-06-27 12:22:30 +0200

    add banners for sponsor, app, team

--- src/components/HomeBanner.vue
+++ src/components/HomeBanner.vue
@@ -11,15 +11,23 @@ export default {
 
 <template>
     <div class="container-fluid m-0 p-0">
-        <div class="row position-relative">
-            <div class="banner_title">
-                {{ banner.title }}
-            </div>
-            <div class="col-12">
-                <img :src="banner.img" alt="" class="" />
-                <div class="row">
-                    <div class="col-6">{{ banner.text }}</div>
-                    <div class="col-6"></div>
+        <div class="row">
+            <div class="col-12 h-100 align-self-center py-5" :style="banner.bg">
+                <div class="row justify-content-evenly text-center p-5">
+                    <div :style="banner.text_style"
+                        class="col-6 col-lg-4 my_text align-self-center d-flex flex-column gap-3">
+                        <div class="banner_title">
+                            {{ banner.title }}
+                        </div>
+                        <div class="div">
+                            {{ banner.text }}
+                        </div>
+                    </div>
+                    <div class="col-6 col-lg-4 my_img">
+                        <a :href="banner.link" target="_blank" rel="noopener noreferrer">
+                            <img class="shadow-lg" :src="banner.img" alt="" />
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -30,17 +38,33 @@ export default {
 .banner_title {
     font-weight: bold;
     font-size: 60px;
-    color: palegoldenrod;
-    position: absolute;
+    color: var(--secondary);
     text-align: center;
+    white-space: nowrap;
 }
 
 .col-12 {
     height: 600px;
+    background-repeat: no-repeat;
+    background-size: cover;
 
-    img {
-        width: 100%;
-        height: 100%;
+    .my_text {
+        font-weight: bold;
+        font-size: large;
+        color: var(--warning);
+    }
+
+    .my_img {
+        img {
+            width: 300px;
+            border-radius: 50%;
+        }
+    }
+}
+
+@media only screen and (max-width: 1200px) {
+    .banner_title {
+        white-space: wrap;
     }
 }
 </style>
